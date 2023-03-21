61dcc3e7c583a2251fe0764edcca9873a813f7b8
trevor james nangosha <trevornangosha16@gmail.com>
2023-03-21 11:36:07 +0300

    visitSuperExpr()

--- src/com/nangosha/lang/AstPrinter.java
+++ src/com/nangosha/lang/AstPrinter.java
@@ -42,6 +42,11 @@ public class AstPrinter implements  Expr.Visitor<String> {
     }
 
     @Override
+    public String visitSuperExpr(Expr.Super expr) {
+        return null;
+    }
+
+    @Override
     public String visitThisExpr(Expr.This expr) {
         return null;
     }
