ae71b4b7c3d89cf5e11dfe52484ae387d468404e
trevor james nangosha <trevornangosha16@gmail.com>
2023-03-21 09:44:31 +0300

    Added support for resolving variables and statements.

--- src/com/nangosha/lang/Lang.java
+++ src/com/nangosha/lang/Lang.java
@@ -59,6 +59,12 @@ public class Lang {
 
         // Stop if there was a syntax error.
         if (hadError) return;
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
+        // Stop if there was a resolution error.
+        if (hadError) return;
+
         interpreter.interpret(statements);
     }
 
